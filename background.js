69205ff977eb305285e15032eb5bbcab5b50e93a
Yash Agrawal <f20211659@pilani.bits-pilani.ac.in>
2023-12-17 13:27:23 +0530

    Added timer and icon

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
+
+chrome.alarms.create('reloadWebsite', { periodInMinutes: 15 });
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'reloadWebsite') {
+    chrome.tabs.query({ url: 'https://nalanda-aws.bits-pilani.ac.in/my/*' }, (tabs) => {
+      tabs.forEach(tab => {
+        chrome.tabs.reload(tab.id);
+      });
+    });
+  }
+});
+
 chrome.action.onClicked.addListener(() => {
   chrome.tabs.create({ url: "https://nalanda-aws.bits-pilani.ac.in/my/" });
 });
